1fbfd406af3a9273816a89bf85dacc07a9817d1a
aliensmart <kaoua17@gmail.com>
2019-09-23 13:17:57 -0400

    t
    NPM and webpack.config.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+function changeGretting(){
+    let greeting = "GoodBye"
+    let elementByTagName = document.getElementsByTagName("h1")
+    elementByTagName[0].textContent = greeting
+}
+
+setTimeout(changeGretting, 5000)
